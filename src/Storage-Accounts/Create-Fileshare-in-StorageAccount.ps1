0fff5cc312b44b5d0ee68996efe5c14447c14078
Rob Hofmann <rob.hofmann@HOFMANN.int>
2020-11-20 16:07:32 +0100

    revert

--- src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
+++ src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
@@ -14,4 +14,4 @@ param (
 . "$PSScriptRoot\..\common\Invoke-Executable.ps1"
 #endregion ===END IMPORTS===
 
-az storage share create --account-name $storageAccountName --name $shareName
+az storage share create --account-name $storageAccountName --name $shareName --auth-mode login
