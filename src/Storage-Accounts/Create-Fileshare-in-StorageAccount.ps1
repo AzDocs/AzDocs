22cd8ec8b2566816f4559f8ab19e0c57a953b4cd
Rob Hofmann <rob.hofmann@HOFMANN.int>
2020-11-20 16:25:06 +0100

    Added accountKey auth mode. CLI giving bad results.

--- src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
+++ src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
@@ -14,4 +14,7 @@ param (
 . "$PSScriptRoot\..\common\Invoke-Executable.ps1"
 #endregion ===END IMPORTS===
 
-az storage share create --account-name $storageAccountName --name $shareName --auth-mode login
+# Somehow we need to use the account key. Managed Identities/integrated security does not seem to work stable over the CLI versions.
+$accountKey = az storage account keys list --resource-group $storageAccountResourceGroupname --account-name $storageAccountName --query=[0].value | ConvertFrom-Json
+
+az storage share create --account-name $storageAccountName --name $shareName --account-key $accountKey
