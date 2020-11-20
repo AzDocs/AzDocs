324b0d42bf97eb9bab369b09c81783ee6dd722b5
Rob Hofmann <rob.hofmann@HOFMANN.int>
2020-11-20 16:01:59 +0100

    Hmm auth mode not supported.

--- src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
+++ src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
@@ -14,4 +14,4 @@ param (
 . "$PSScriptRoot\..\common\Invoke-Executable.ps1"
 #endregion ===END IMPORTS===
 
-az storage share create --account-name $storageAccountName --name $shareName --auth-mode login
+az storage share create --account-name $storageAccountName --name $shareName
