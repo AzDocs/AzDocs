74de3d263c0057def538ac89092452dbc87278f6
Rob Hofmann <rob.hofmann@HOFMANN.int>
2020-11-23 18:42:41 +0100

    Whitelist added to fileshare when using vnet setup

--- src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
+++ src/Storage-Accounts/Create-Fileshare-in-StorageAccount.ps1
@@ -1,18 +1,42 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $storageAccountResourceGroupname,
 
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $storageAccountName,
 
+    [Parameter(Mandatory)]
+    [String] $shareName,
+
     [Parameter()]
-    [String] $shareName
+    [String] $vnetName,
+
+    [Parameter()]
+    [String] $vnetResourceGroupName,
+
+    [Parameter()]
+    [String] $subnetToWhitelist,
+
+    [Parameter()]
+    [Switch] $storageAccountIsInVnet
 )
 
 #region ===BEGIN IMPORTS===
 . "$PSScriptRoot\..\common\Invoke-Executable.ps1"
+. "$PSScriptRoot\..\common\Set-SubnetServiceEndpoint.ps1"
 #endregion ===END IMPORTS===
+
+if($storageAccountIsInVnet)
+{
+    $subnetToWhitelistId = (Invoke-Executable az network vnet subnet show -g $vnetResourceGroupName -n $subnetToWhitelist --vnet-name $vnetName | ConvertFrom-Json).id
+
+    # Add Service Endpoint to the Subnet to make sure we can connect to the service within the VNET
+    Set-SubnetServiceEndpoint -SubnetResourceId $subnetToWhitelistId -ServiceName "Microsoft.Storage"
+
+    # Whitelist our App's subnet in the storage account so we can connect
+    Invoke-Executable az storage account network-rule add --resource-group $storageAccountResourceGroupname --account-name $storageAccountName --subnet $subnetToWhitelistId
+}
 
 # Somehow we need to use the account key. Managed Identities/integrated security does not seem to work stable over the CLI versions.
 $accountKey = Invoke-Executable az storage account keys list --resource-group $storageAccountResourceGroupname --account-name $storageAccountName --query=[0].value | ConvertFrom-Json
