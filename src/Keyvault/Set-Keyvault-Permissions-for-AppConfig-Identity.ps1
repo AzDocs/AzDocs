cdfe4573f47af8dac4d8a673eaf880402c7a044f
Jacob Struiksma <jacob.struiksma@kpn.com>
2020-12-21 11:46:43 +0100

    adding slot parameter for  keyvault permissions

--- src/Keyvault/Set-Keyvault-Permissions-for-AppConfig-Identity.ps1
+++ src/Keyvault/Set-Keyvault-Permissions-for-AppConfig-Identity.ps1
@@ -1,32 +1,40 @@
 [CmdletBinding()]
 param (
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $appConfigName,
 
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $appConfigResourceGroupName,
 
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $keyvaultCertificatePermissions,
 
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $keyvaultKeyPermissions,
 
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $keyvaultSecretPermissions,
 
-    [Parameter()]
+    [Parameter(Mandatory)]
     [String] $keyvaultStoragePermissions,
 
+    [Parameter(Mandatory)]
+    [String] $keyvaultName,
+
     [Parameter()]
-    [String] $keyvaultName
+    [String] $AppServiceSlotName
 )
 
 #region ===BEGIN IMPORTS===
 . "$PSScriptRoot\..\common\Invoke-Executable.ps1"
 #endregion ===END IMPORTS===
 
-$identityId = (Invoke-Executable az appconfig identity show --resource-group $appConfigResourceGroupName --name $appConfigName | ConvertFrom-Json).principalId
+$additionalParameters = @()
+if ($AppServiceSlotName) {
+    $additionalParameters += '--slot' , $AppServiceSlotName
+}
+
+$identityId = (Invoke-Executable az appconfig identity show --resource-group $appConfigResourceGroupName --name $appConfigName @additionalParameters | ConvertFrom-Json).principalId
 if (-not $identityId) {
     throw "Could not find identity for $appConfigName"
 }
@@ -34,7 +42,7 @@ Write-Host "Identity ID: $identityId"
 
 $kvcp = $keyvaultCertificatePermissions -split ' '
 $kvkp = $keyvaultKeyPermissions -split ' '
-$kvsp =   $keyvaultSecretPermissions -split ' '
-$kvstp  = $keyvaultStoragePermissions -split ' '
+$kvsp = $keyvaultSecretPermissions -split ' '
+$kvstp = $keyvaultStoragePermissions -split ' '
 
 Invoke-Executable az keyvault set-policy --certificate-permissions @kvcp --key-permissions @kvkp --secret-permissions @kvsp --storage-permissions @kvstp --object-id $identityId --name $keyvaultName
