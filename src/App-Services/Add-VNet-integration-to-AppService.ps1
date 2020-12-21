db50989dd0d3e1067c04707094a4b182545cd6ee
Jacob Struiksma <jacob.struiksma@kpn.com>
2020-12-21 13:02:55 +0100

    adding slot support for vnet integration

--- src/App-Services/Add-VNet-integration-to-AppService.ps1
+++ src/App-Services/Add-VNet-integration-to-AppService.ps1
@@ -10,16 +10,31 @@ param (
     [string] $vnetName,
 
     [Parameter(Mandatory)]
-    [string] $appServiceVnetIntegrationSubnetName
+    [string] $appServiceVnetIntegrationSubnetName,
+
+    [Parameter()]
+    [string] $AppServiceSlotName
 )
 
 #region ===BEGIN IMPORTS===
 . "$PSScriptRoot\..\common\Invoke-Executable.ps1"
 #endregion ===END IMPORTS===
 
+$fullAppServiceName = $appServiceName
+$additionalParameters = @()
 
-if ((invoke-Executable az webapp vnet-integration list --resource-group $appServiceResourceGroupName --name $appServiceName).length -le 2) {
-    Write-Host "VNET Integration not found, adding it to $appServiceName"
-    invoke-Executable az webapp vnet-integration add --resource-group $appServiceResourceGroupName --name $appServiceName --vnet $vnetName --subnet $appServiceVnetIntegrationSubnetName
-    invoke-Executable az webapp restart --name $appServiceName --resource-group $appServiceResourceGroupName
+if ($AppServiceSlotName) {
+    $additionalParameters += '--slot' , $AppServiceSlotName
+    $fullAppServiceName += " [$AppServiceSlotName]"
+}
+
+$data = az webapp vnet-integration list --resource-group $appServiceResourceGroupName --name $appServiceName @additionalParameters | ConvertFrom-Json
+
+if ($data) {
+    Write-Host "VNET Integration found for $fullAppServiceName"
+}
+else {
+    Write-Host "VNET Integration NOT found, adding it to $fullAppServiceName"
+    invoke-Executable az webapp vnet-integration add --resource-group $appServiceResourceGroupName --name $appServiceName --vnet $vnetName --subnet $appServiceVnetIntegrationSubnetName @additionalParameters
+    invoke-Executable az webapp restart --name $appServiceName --resource-group $appServiceResourceGroupName @additionalParameters
 }
