562f4360608d41f916b7b3db50d5316530ec9e1c
Jacob Struiksma <jacob.struiksma@kpn.com>
2020-12-23 12:55:48 +0100

    adjust colors for azure pipeline stuff

--- src/common/Write-HeaderFooter.ps1
+++ src/common/Write-HeaderFooter.ps1
@@ -1,20 +1,66 @@
+function Write-ColorHost {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $Message,
+
+        [Parameter()]
+        [validateset('Regular', 'BeginGroup', 'EndGroup', 'Background')]
+        [string]
+        $Type = 'Regular'
+    )
+    if ( $Type -eq 'Regular') {
+        Write-Host $Message
+    }
+    if ($env:System.HostType) {
+        switch ($Type) {
+
+            'BeginGroup' {
+                Write-Host "##[group]> $Message"
+            }
+            'EndGroup' {
+                Write-Host "##[endgroup]> $Message"
+            }
+            'Background' {
+                Write-Host "##[section]> $Message"
+            }
+            Default {}
+        }
+    }
+    else {
+
+        switch -wildcard ($Type) {
+
+            '*Group' {
+                Write-Host $Message -ForegroundColor Green
+            }
+            'Background' {
+                Write-Host $Message  -ForegroundColor DarkGray
+            }
+            Default {}
+        }
+    }
+
+}
+
 function Write-Header {
     [System.Management.Automation.InvocationInfo]$myInvocation = Get-Variable -Name "MyInvocation" -Scope 1 -ValueOnly
-    Write-Host "> $($myInvocation.InvocationName)"  -ForegroundColor Green
+    Write-ColorHost "> $($myInvocation.InvocationName)" -Type 'BeginGroup'
 
     [System.Management.Automation.PSCmdlet]$headerCmdlet = Get-Variable -Name "PSCmdlet" -Scope 1 -ValueOnly
 
     if ($headerCmdlet.ParameterSetName -ne '__AllParameterSets') {
-        Write-Host ">  ParameterSetName : $($headerCmdlet.ParameterSetName)"  -ForegroundColor DarkGray
+        Write-ColorHost ">  ParameterSetName : $($headerCmdlet.ParameterSetName)" -Type 'Background'
     }
 
     $myInvocation.BoundParameters.Keys | ForEach-Object {
         $kv = $myInvocation.BoundParameters[$_]
-        Write-Host ">  $_ : $kv"  -ForegroundColor DarkGray
+        Write-ColorHost ">  $_ : $kv" -Type 'Background'
     }
 }
 
 function Write-Footer {
     [System.Management.Automation.InvocationInfo]$myInvocation = Get-Variable -Name "MyInvocation" -Scope 1 -ValueOnly
-    Write-Host "< $($myInvocation.InvocationName)"  -ForegroundColor Green
+    Write-ColorHost "< $($myInvocation.InvocationName)" -Type 'EndGroup'
 }
