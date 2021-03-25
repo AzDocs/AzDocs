aec821c9c71302353869662747477f9adb5b120b
Rob Hofmann <rob.hofmann@outlook.com>
2021-03-25 12:28:18 +0100

    Set-Application-Gateway-SSLTLS-Settings

---
+++ Wiki/Azure/Azure-CLI-Snippets/Application-Gateway/Set-Application-Gateway-SSLTLS-Settings.md
@@ -0,0 +1,21 @@
+[[_TOC_]]
+
+# Description
+This snippet will set Ciphers & the minimal TLS version on your AppGateway. If you use the defaults, it will set to TLS 1.2 & a set of non-compromised strong cipher suites. All devices/browsers/os's newer than february 2014 should support this default set.
+
+# Parameters
+Some parameters from [General Parameter](/Azure/Azure-CLI-Snippets) list.
+
+| Parameter | Example Value | Description |
+|--|--|--|
+| ApplicationGatewayName | `customer-appgw-$(Release.EnvironmentName)` | The name to use for this application gateway |
+| ApplicationGatewayResourceGroupName | `myshared-resourcegroup` | The name of the resourcegroup to place this application gateway in. |
+| ApplicationGatewayMinimalProtocolVersion | `TLSv1_2` | The minimal TLS version to use. The default is TLS 1.2. It is extremely recommended to use TLS 1.2 or higher at the point of writing. Current options: `TLSv1_0`, `TLSv1_1`, `TLSv1_2`. For all (up-to-date) options use `az network application-gateway ssl-policy list-options`. |
+| ApplicationGatewayCipherSuites | `@('TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256','TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384')` | The set of ciphers to be allowed/used on the Application Gateway. This defaults to a set of ciphers which are (at the point of writing this) found secure & non-compromisable. For options, please use `az network application-gateway ssl-policy list-options`. |
+
+# Code
+[Click here to download this script](../../../../src/Application-Gateway/Set-Application-Gateway-SSLTLS-Settings.ps1)
+
+# Links
+
+[Azure CLI - az network application-gateway ssl-policy set](https://docs.microsoft.com/en-us/cli/azure/network/application-gateway/ssl-policy?view=azure-cli-latest#az_network_application_gateway_ssl_policy_set)
---
+++ src/Application-Gateway/Set-Application-Gateway-SSLTLS-Settings.ps1
@@ -0,0 +1,57 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)][string] $ApplicationGatewayName,
+    [Parameter(Mandatory)][string] $ApplicationGatewayResourceGroupName,
+    [Parameter()][ValidateSet('TLSv1_0', 'TLSv1_1', 'TLSv1_2')][string] $ApplicationGatewayMinimalProtocolVersion = 'TLSv1_2',
+    [Parameter()][ValidateSet(
+        'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+        'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+        'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384',
+        'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256',
+        'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA',
+        'TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA',
+        'TLS_DHE_RSA_WITH_AES_256_GCM_SHA384',
+        'TLS_DHE_RSA_WITH_AES_128_GCM_SHA256',
+        'TLS_DHE_RSA_WITH_AES_256_CBC_SHA',
+        'TLS_DHE_RSA_WITH_AES_128_CBC_SHA',
+        'TLS_RSA_WITH_AES_256_GCM_SHA384',
+        'TLS_RSA_WITH_AES_128_GCM_SHA256',
+        'TLS_RSA_WITH_AES_256_CBC_SHA256',
+        'TLS_RSA_WITH_AES_128_CBC_SHA256',
+        'TLS_RSA_WITH_AES_256_CBC_SHA',
+        'TLS_RSA_WITH_AES_128_CBC_SHA',
+        'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384',
+        'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256',
+        'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384',
+        'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256',
+        'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA',
+        'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA',
+        'TLS_DHE_DSS_WITH_AES_256_CBC_SHA256',
+        'TLS_DHE_DSS_WITH_AES_128_CBC_SHA256',
+        'TLS_DHE_DSS_WITH_AES_256_CBC_SHA',
+        'TLS_DHE_DSS_WITH_AES_128_CBC_SHA',
+        'TLS_RSA_WITH_3DES_EDE_CBC_SHA',
+        'TLS_DHE_DSS_WITH_3DES_EDE_CBC_SHA'
+    )][string[]] $ApplicationGatewayCipherSuites = @(
+        'TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256',
+        'TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384',
+        'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA',
+        'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA',
+        'TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256',
+        'TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA384',
+        'TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384',
+        'TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256',
+        'TLS_RSA_WITH_AES_256_GCM_SHA384',
+        'TLS_RSA_WITH_AES_128_GCM_SHA256'
+    )
+)
+
+#region ===BEGIN IMPORTS===
+Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
+#endregion ===END IMPORTS===
+
+Write-Header -ScopedPSCmdlet $PSCmdlet
+
+Invoke-Executable az network application-gateway ssl-policy set --resource-group $ApplicationGatewayResourceGroupName --gateway-name $ApplicationGatewayName --policy-type Custom --min-protocol-version $MinimalProtocolVersion --cipher-suites @CipherSuites
+
+Write-Footer -ScopedPSCmdlet $PSCmdlet
