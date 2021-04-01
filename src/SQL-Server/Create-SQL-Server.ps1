cf71d9676701247ad3b0882ef4174ecb6785ed41
Rob Hofmann <rob.hofmann@outlook.com>
2021-04-01 17:05:55 +0200

    test

--- src/SQL-Server/Create-SQL-Server.ps1
+++ src/SQL-Server/Create-SQL-Server.ps1
@@ -35,7 +35,10 @@ Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
 Write-Header -ScopedPSCmdlet $PSCmdlet
 
 # Create SQL Server
-Invoke-Executable az sql server create --admin-password $SqlServerPassword --admin-user $SqlServerUsername --name $SqlServerName --resource-group $SqlServerResourceGroupName --enable-public-network $SqlServerEnablePublicNetwork --minimal-tls-version $SqlServerMinimalTlsVersion
+if (!(Invoke-Executable az sql server show --name $SqlServerName --resource-group $SqlServerResourceGroupName))
+{
+    Invoke-Executable az sql server create --admin-password $SqlServerPassword --admin-user $SqlServerUsername --name $SqlServerName --resource-group $SqlServerResourceGroupName --enable-public-network $SqlServerEnablePublicNetwork --minimal-tls-version $SqlServerMinimalTlsVersion
+}
 
 # Fetch the resource id for the just created SQL Server
 $sqlServerId = (Invoke-Executable az sql server show --name $SqlServerName --resource-group $SqlServerResourceGroupName | ConvertFrom-Json).id
