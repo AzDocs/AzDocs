b5a2101d528db9452feccae4620381335d2edba6
Rob Hofmann <rob.hofmann@outlook.com>
2021-05-27 09:44:41 +0200

    networking functions corrected.

--- src/AzDocs.Common/public/Networking-Helper-Functions.ps1
+++ src/AzDocs.Common/public/Networking-Helper-Functions.ps1
@@ -6,18 +6,25 @@ function Get-StartIpInIpv4Network
         [Parameter(Mandatory)][string] $SubnetCidr
     )
 
-    $networkAddress = ($SubnetCidr.split("/"))[0]
-    $networkIp = ([System.Net.IPAddress]$networkAddress).GetAddressBytes()
-    [Array]::Reverse($networkIp)
-    $networkIp = ([System.Net.IPAddress]($networkIp -join ".")).Address
-    $startIP = $networkIp +1
+    $splittedIpAddress = $SubnetCidr -split '/'
+    $subnetMask = ConvertTo-IPv4MaskString -MaskBits $splittedIpAddress[1]
 
-    if (($startIP.Gettype()).Name -ine "double")
+    $BinarySubnetMask = ""
+    if($splittedIpAddress[1] -ne "32")
     {
-        $startIP = [Convert]::ToDouble($startIP)
+        $BinarySubnetMask = (Convert-SubnetMask-To-Binary $subnetMask).replace(".", "")
     }
-    $startIP = [System.Net.IPAddress]$startIP
-    return $startIP.IPAddressToString
+    $BinaryNetworkAddressSection = $BinarySubnetMask.replace("1", "")
+    $BinaryNetworkAddressLength = $BinaryNetworkAddressSection.length
+    $CIDR = 32 - $BinaryNetworkAddressLength
+    $BinaryIP = (Convert-IP-To-Binary $splittedIpAddress[0]).Replace(".", "")
+    $BinaryIPNetworkSection = $BinaryIP.substring(0, $CIDR)
+    
+    #Starting IP
+    $FirstAddress = $BinaryNetworkAddressSection -replace "0$", "1"
+    $BinaryFirstAddress = $BinaryIPNetworkSection + $FirstAddress
+    $strFirstIP = Convert-BinaryIPAddress $BinaryFirstAddress
+    return $strFirstIP
 }
 
 function Get-EndIpInIpv4Network
@@ -28,20 +35,180 @@ function Get-EndIpInIpv4Network
         [Parameter(Mandatory)][string] $SubnetCidr
     )
 
-    $networkAddress = ($SubnetCidr.split("/"))[0]
-    $networkLength = ($SubnetCidr.split("/"))[1]
-    $IpLength = 32-$networkLength
-    $numberOfIps = ([System.Math]::Pow(2, $IpLength)) -2
-    $networkIp = ([System.Net.IPAddress]$networkAddress).GetAddressBytes()
-    [Array]::Reverse($networkIp)
-    $networkIp = ([System.Net.IPAddress]($networkIp -join ".")).Address
-    $endIp = $networkIp + $numberOfIps
-    
-    if (($endIp.Gettype()).Name -ine "double")
-    {
-        $endIp = [Convert]::ToDouble($endIp)
-    }
-    $endIp = [System.Net.IPAddress]$endIp
+    $splittedIpAddress = $SubnetCidr -split '/'
+    $subnetMask = ConvertTo-IPv4MaskString -MaskBits $splittedIpAddress[1]
 
-    return $endIp.IPAddressToString
+    $BinarySubnetMask = ""
+    if($splittedIpAddress[1] -ne "32")
+    {
+        $BinarySubnetMask = (Convert-SubnetMask-To-Binary $subnetMask).replace(".", "")
+    }
+    $BinaryNetworkAddressSection = $BinarySubnetMask.replace("1", "")
+    $BinaryNetworkAddressLength = $BinaryNetworkAddressSection.length
+    $CIDR = 32 - $BinaryNetworkAddressLength
+    $BinaryIP = (Convert-IP-To-Binary $splittedIpAddress[0]).Replace(".", "")
+    $BinaryIPNetworkSection = $BinaryIP.substring(0, $CIDR)
+       
+    #End IP
+    $LastAddress = ($BinaryNetworkAddressSection -replace "0", "1") -replace "1$", "0"
+    $BinaryLastAddress = $BinaryIPNetworkSection + $LastAddress
+    $strLastIP = Convert-BinaryIPAddress $BinaryLastAddress
+    return $strLastIP
+}
+
+
+
+function ConvertTo-IPv4MaskString {
+    param(
+      [Parameter(Mandatory = $true)]
+      [ValidateRange(0, 32)]
+      [Int] $MaskBits
+    )
+    $mask = ([Math]::Pow(2, $MaskBits) - 1) * [Math]::Pow(2, (32 - $MaskBits))
+    $bytes = [BitConverter]::GetBytes([UInt32] $mask)
+    (($bytes.Count - 1)..0 | ForEach-Object { [String] $bytes[$_] }) -join "."
+}
+
+
+function Validate-IP ($strIP)
+{
+	$bValidIP = $true
+	$arrSections = @()
+	$arrSections +=$strIP.split(".")
+	#firstly, make sure there are 4 sections in the IP address
+	if ($arrSections.count -ne 4) {$bValidIP =$false}
+	
+	#secondly, make sure it only contains numbers and it's between 0-254
+	if ($bValidIP)
+	{
+		[reflection.assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+		foreach ($item in $arrSections)
+		{
+			if (!([Microsoft.VisualBasic.Information]::isnumeric($item))) {$bValidIP = $false}
+		}
+	}
+	
+	if ($bValidIP)
+	{
+		foreach ($item in $arrSections)
+		{
+			$item = [int]$item
+			if ($item -lt 0 -or $item -gt 254) {$bValidIP = $false}
+		}
+	}
+	
+	Return $bValidIP
+}
+
+function Validate-SubnetMask ($strSubnetMask)
+{
+	$bValidMask = $true
+	$arrSections = @()
+	$arrSections +=$strSubnetMask.split(".")
+	#firstly, make sure there are 4 sections in the subnet mask
+	if ($arrSections.count -ne 4) {$bValidMask =$false}
+	
+	#secondly, make sure it only contains numbers and it's between 0-255
+	if ($bValidMask)
+	{
+		[reflection.assembly]::LoadWithPartialName("Microsoft.VisualBasic") | Out-Null
+		foreach ($item in $arrSections)
+		{
+			if (!([Microsoft.VisualBasic.Information]::isnumeric($item))) {$bValidMask = $false}
+		}
+	}
+	
+	if ($bValidMask)
+	{
+		foreach ($item in $arrSections)
+		{
+			$item = [int]$item
+			if ($item -lt 0 -or $item -gt 255) {$bValidMask = $false}
+		}
+	}
+	
+	#lastly, make sure it is actually a subnet mask when converted into binary format
+	if ($bValidMask)
+	{
+		foreach ($item in $arrSections)
+		{
+			$binary = [Convert]::ToString($item,2)
+			if ($binary.length -lt 8)
+			{
+				do {
+				$binary = "0$binary"
+				} while ($binary.length -lt 8)
+			}
+			$strFullBinary = $strFullBinary+$binary
+		}
+		if ($strFullBinary.contains("01")) {$bValidMask = $false}
+		if ($bValidMask)
+		{
+			$strFullBinary = $strFullBinary.replace("10", "1.0")
+			if ((($strFullBinary.split(".")).count -ne 2)) {$bValidMask = $false}
+		}
+	}
+	Return $bValidMask
+}
+
+function ConvertTo-Binary ($strDecimal)
+{
+	$strBinary = [Convert]::ToString($strDecimal, 2)
+	if ($strBinary.length -lt 8)
+	{
+		while ($strBinary.length -lt 8)
+		{
+			$strBinary = "0"+$strBinary
+		}
+	}
+	Return $strBinary
+}
+function Convert-IP-To-Binary ($strIP)
+{
+	$strBinaryIP = $null
+	if (Validate-IP $strIP)
+	{
+		$arrSections = @()
+		$arrSections += $strIP.split(".")
+		foreach ($section in $arrSections)
+		{
+			if ($strBinaryIP -ne $null)
+			{
+				$strBinaryIP = $strBinaryIP+"."
+			}
+				$strBinaryIP = $strBinaryIP+(ConvertTo-Binary $section)
+			
+		}
+	}
+	Return $strBinaryIP
+}
+
+Function Convert-SubnetMask-To-Binary ($strSubnetMask)
+{
+		$strBinarySubnetMask = $null
+	if (Validate-SubnetMask $strSubnetMask)
+	{
+		$arrSections = @()
+		$arrSections += $strSubnetMask.split(".")
+		foreach ($section in $arrSections)
+		{
+			if ($strBinarySubnetMask -ne $null)
+			{
+				$strBinarySubnetMask = $strBinarySubnetMask+"."
+			}
+				$strBinarySubnetMask = $strBinarySubnetMask+(ConvertTo-Binary $section)
+			
+		}
+	}
+	Return $strBinarySubnetMask
+}
+
+Function Convert-BinaryIPAddress ($BinaryIP)
+{
+	$FirstSection = [Convert]::ToInt64(($BinaryIP.substring(0, 8)),2)
+	$SecondSection = [Convert]::ToInt64(($BinaryIP.substring(8,8)),2)
+	$ThirdSection = [Convert]::ToInt64(($BinaryIP.substring(16,8)),2)
+	$FourthSection = [Convert]::ToInt64(($BinaryIP.substring(24,8)),2)
+	$strIP = "$FirstSection`.$SecondSection`.$ThirdSection`.$FourthSection"
+	Return $strIP
 }
