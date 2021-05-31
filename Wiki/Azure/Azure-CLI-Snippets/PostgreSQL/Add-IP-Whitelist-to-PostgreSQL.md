c9223e8e343709c91a45be33861cafe6a5f5b263
Rob Hofmann <rob.hofmann@outlook.com>
2021-05-31 17:04:36 +0200

    postgresql support

---
+++ Wiki/Azure/Azure-CLI-Snippets/PostgreSQL/Add-IP-Whitelist-to-PostgreSQL.md
@@ -0,0 +1,19 @@
+[[_TOC_]]
+
+# Description
+This snippet will whitelist the specified IP Range from the Azure Database for PostgreSQL server. If you leave the `CIDRToWhitelist` parameter empty, it will use the outgoing IP from where you execute this script.
+
+# Parameters
+| Parameter | Required | Example Value | Description |
+|--|--|--|--|
+| PostgreSqlServerResourceGroupName | <input type="checkbox" checked> | `myteam-testapi-$(Release.EnvironmentName)` | The name of the resource group the PostgreSQL Server is in. |
+| PostgreSqlServerName | <input type="checkbox" checked> | `somepostgresqlserver$(Release.EnvironmentName)` | The name for the PostgreSQL Server resource. It's recommended to use just alphanumerical characters without hyphens etc. |
+| AccessRuleName | <input type="checkbox"> | `company hq` | You can override the name for this accessrule. If you leave this empty, the `CIDRToWhitelist` will be used for the naming (automatically). We recommend to leave this empty for ephemeral whitelists like Azure DevOps Hosted Agent ip's. |
+| CIDRToWhitelist | <input type="checkbox"> | `52.43.65.123/32` | IP range in [CIDR](https://en.wikipedia.org/wiki/Classless_Inter-Domain_Routing) notation that should be whitelisted. If you leave this value empty, it will whitelist the machine's ip where you're running the script from. |
+
+# Code
+[Click here to download this script](../../../../src/PostgreSQL/Add-IP-Whitelist-to-PostgreSQL.ps1)
+
+# Links
+
+[Azure CLI - az postgres server firewall-rule create](https://docs.microsoft.com/en-us/cli/azure/postgres/server/firewall-rule?view=azure-cli-latest#az_postgres_server_firewall_rule_create)
---
+++ src/PostgreSQL/Remove-IP-Whitelist-from-PosgreSQL.ps1
@@ -0,0 +1,40 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory)][string] $PosgreSqlServerName,
+    [Parameter(Mandatory)][string] $PosgreSqlServerResourceGroupName,
+    [Parameter()][string] $AccessRuleName,
+    [Parameter()][ValidatePattern('^$|^(?:(?:\d{1,3}.){3}\d{1,3})\/(?:\d{1,2})$', ErrorMessage = "The text '{0}' does not match with the CIDR notation, like '1.2.3.4/32'")][string] $CIDRToRemoveFromWhitelist
+)
+
+#region ===BEGIN IMPORTS===
+Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
+#endregion ===END IMPORTS===
+
+Write-Header -ScopedPSCmdlet $PSCmdlet
+
+if(!$CIDRToRemoveFromWhitelist -and !$AccessRuleName)
+{
+    $response  = Invoke-WebRequest 'https://ipinfo.io/ip'
+    $CIDRToRemoveFromWhitelist = $response.Content.Trim() + '/32'
+}
+
+if(!$AccessRuleName)
+{
+    $startIpAddress = Get-StartIpInIpv4Network -SubnetCidr $CIDRToRemoveFromWhitelist
+    $endIpAddress = Get-EndIpInIpv4Network -SubnetCidr $CIDRToRemoveFromWhitelist
+    $firewallRules = Invoke-Executable az postgres server firewall-rule list --resource-group $PosgreSqlServerResourceGroupName --server-name $PosgreSqlServerName | ConvertFrom-Json
+    $matchingFirewallRule = $firewallRules | Where-Object { $_.startIpAddress -eq $startIpAddress -and $_.endIpAddress -eq $endIpAddress }
+    if($matchingFirewallRule)
+    {
+        $AccessRuleName = $matchingFirewallRule.name
+    }
+    else
+    {
+        throw "Could not find a matching accessrule to delete."
+    }
+}
+
+# Execute whitelist
+Invoke-Executable az postgres server firewall-rule delete --resource-group $PosgreSqlServerResourceGroupName --server-name $PosgreSqlServerName --name $AccessRuleName --yes
+
+Write-Footer -ScopedPSCmdlet $PSCmdlet
