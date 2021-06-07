4f98d412d59da2863e72d04d5977273f19497ff2
Lotte Voorhorst <lotte.voorhorst@kpn.com>
2021-06-07 13:49:35 +0200

    wip storage account

--- src/Storage-Accounts/Add-IP-Whitelist-to-StorageAccount.ps1
+++ src/Storage-Accounts/Add-IP-Whitelist-to-StorageAccount.ps1
@@ -2,8 +2,13 @@
 param (
     [Parameter(Mandatory)][string] $StorageAccountName,
     [Parameter(Mandatory)][string] $StorageAccountResourceGroupName,
-    [Parameter()][ValidatePattern('^$|^(?:(?:\d{1,3}.){3}\d{1,3})(?:\/(?:\d{1,2}))?$', ErrorMessage = "The text '{0}' does not match with the CIDR notation, like '1.2.3.4/32'")][string] $CIDRToWhitelist
+    [Parameter()][ValidatePattern('^$|^(?:(?:\d{1,3}.){3}\d{1,3})(?:\/(?:\d{1,2}))?$', ErrorMessage = "The text '{0}' does not match with the CIDR notation, like '1.2.3.4/32'")][string] $CIDRToWhitelist,
+    [Parameter()][string] $SubnetName,
+    [Parameter()][string] $VnetName,
+    [Parameter()][string] $VnetResourceGroupName
 )
+
+# TODO: REMOVE => Storage account update de waarden. Geen dubbele waarden aanwezig. Geen issues met bestaat al.
 
 #region ===BEGIN IMPORTS===
 Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
@@ -11,12 +16,34 @@ Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
 
 Write-Header -ScopedPSCmdlet $PSCmdlet
 
-if (!$CIDRToWhitelist)
+if ($CIDRToWhitelist -and $SubnetName -and $VnetName -and $VnetResourceGroupName)
+{
+    throw "You can not enter a CIDRToWhitelist (CIDR whitelisting) in combination with SubnetName, VnetName, VnetResourceGroupName (Subnet whitelisting). Choose one of the two options."
+}
+
+# Autogenerate CIDR if no CIDR or Subnet is passed
+if (!$CIDRToWhitelist -and (!$SubnetName -or !$VnetName -or !$VnetResourceGroupName))
 {
     $response = Invoke-WebRequest 'https://ipinfo.io/ip'
     $CIDRToWhitelist = $response.Content.Trim()
 }
 
-Invoke-Executable az storage account network-rule add --account-name $StorageAccountName --resource-group $StorageAccountResourceGroupName --ip-address $CIDRToWhitelist
+# Fetch Subnet ID when subnet option is given.
+if ($SubnetName -and $VnetName -and $VnetResourceGroupName)
+{
+    $subnetResourceId = (Invoke-Executable az network vnet subnet show --resource-group $VnetResourceGroupName --name $SubnetName --vnet-name $VnetName | ConvertFrom-Json).id
+}
+
+$optionalParameters = @()
+if ($CIDRToWhitelist)
+{
+    $optionalParameters += "--ip-address", "$CIDRToWhitelist"
+}
+elseif ($subnetResourceId)
+{
+    $optionalParameters += "--subnet", "$subnetResourceId"
+}
+
+Invoke-Executable az storage account network-rule add --account-name $StorageAccountName --resource-group $StorageAccountResourceGroupName @optionalParameters
 
 Write-Footer -ScopedPSCmdlet $PSCmdlet
--- src/Storage-Accounts/Remove-IP-Whitelist-from-StorageAccount.ps1
+++ src/Storage-Accounts/Remove-IP-Whitelist-from-StorageAccount.ps1
@@ -2,7 +2,10 @@
 param (
     [Parameter(Mandatory)][string] $StorageAccountName,
     [Parameter(Mandatory)][string] $StorageAccountResourceGroupName,
-    [Parameter()][ValidatePattern('^$|^(?:(?:\d{1,3}.){3}\d{1,3})(?:\/(?:\d{1,2}))?$', ErrorMessage = "The text '{0}' does not match with the CIDR notation, like '1.2.3.4/32'")][string] $CIDRToRemoveFromWhitelist
+    [Parameter()][ValidatePattern('^$|^(?:(?:\d{1,3}.){3}\d{1,3})(?:\/(?:\d{1,2}))?$', ErrorMessage = "The text '{0}' does not match with the CIDR notation, like '1.2.3.4/32'")][string] $CIDRToRemoveFromWhitelist,
+    [Parameter()][string] $SubnetName,
+    [Parameter()][string] $VnetName,
+    [Parameter()][string] $VnetResourceGroupName
 )
 
 #region ===BEGIN IMPORTS===
@@ -11,12 +14,34 @@ Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
 
 Write-Header -ScopedPSCmdlet $PSCmdlet
 
-if(!$CIDRToRemoveFromWhitelist)
+if ($CIDRToRemoveFromWhitelist -and $SubnetName -and $VnetName -and $VnetResourceGroupName)
 {
-    $response  = Invoke-WebRequest 'https://ipinfo.io/ip'
+    throw "You can not enter a CIDRToWhitelist (CIDR whitelisting) in combination with SubnetName, VnetName, VnetResourceGroupName (Subnet whitelisting). Choose one of the two options."
+}
+
+# Autogenerate CIDR if no CIDR or Subnet is passed
+if (!$CIDRToRemoveFromWhitelist -and (!$SubnetName -or !$VnetName -or !$VnetResourceGroupName))
+{
+    $response = Invoke-WebRequest 'https://ipinfo.io/ip'
     $CIDRToRemoveFromWhitelist = $response.Content.Trim()
 }
 
-Invoke-Executable az storage account network-rule remove --account-name $StorageAccountName --resource-group $StorageAccountResourceGroupName --ip-address $CIDRToRemoveFromWhitelist
+# Fetch Subnet ID when subnet option is given.
+if ($SubnetName -and $VnetName -and $VnetResourceGroupName)
+{
+    $subnetResourceId = (Invoke-Executable az network vnet subnet show --resource-group $VnetResourceGroupName --name $SubnetName --vnet-name $VnetName | ConvertFrom-Json).id
+}
+
+$optionalParameters = @()
+if ($CIDRToRemoveFromWhitelist)
+{
+    $optionalParameters += "--ip-address", "$CIDRToRemoveFromWhitelist"
+}
+elseif ($subnetResourceId)
+{
+    $optionalParameters += "--subnet", "$subnetResourceId"
+}
+
+Invoke-Executable az storage account network-rule remove --account-name $StorageAccountName --resource-group $StorageAccountResourceGroupName @optionalParameters
 
 Write-Footer -ScopedPSCmdlet $PSCmdlet
