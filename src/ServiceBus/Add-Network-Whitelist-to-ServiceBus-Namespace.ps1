cf79393e143542d4889e9faa1066c05532c74301
Simon van der Linde-Bults <simon.vanderlinde-bults@sycade.com>
2021-07-27 13:59:20 +0200

    Check for servicebus sku

--- src/ServiceBus/Add-Network-Whitelist-to-ServiceBus-Namespace.ps1
+++ src/ServiceBus/Add-Network-Whitelist-to-ServiceBus-Namespace.ps1
@@ -14,36 +14,46 @@ Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
 
 Write-Header -ScopedPSCmdlet $PSCmdlet
 
-# Confirm if the correct parameters are passed
-Confirm-ParametersForWhitelist -CIDR:$CIDRToWhitelist -SubnetName:$SubnetToWhitelistSubnetName -VnetName:$SubnetToWhitelistVnetName -VnetResourceGroupName:$SubnetToWhitelistVnetResourceGroupName
+# Get ServiceBus SKU. Can only be set for Premium tier.
+$ServiceBusSku = (Invoke-Executable az servicebus namespace show --name $ServiceBusNamespaceName --resource-group $ServiceBusNamespaceResourceGroupName | ConvertFrom-Json).sku.tier
 
-# Autogenerate CIDR if no CIDR or Subnet is passed
-
-$CIDRToWhitelist = Get-CIDRForWhitelist -CIDR:$CIDRToWhitelist -CIDRSuffix '/32' -SubnetName:$SubnetToWhitelistSubnetName -VnetName:$SubnetToWhitelistVnetName -VnetResourceGroupName:$SubnetToWhitelistVnetResourceGroupName
-
-# Github issue for being able to add the same ip-addressess: https://github.com/Azure/azure-cli/issues/18479
-# Have to add suffix because of bug: 
-$CIDRToWhitelist = Confirm-CIDRForWhitelist -ServiceType 'servicebus' -CIDR:$CIDRToWhitelist
-
-# Fetch Subnet ID when subnet option is given.
-if ($SubnetToWhitelistSubnetName -and $SubnetToWhitelistVnetName -and $SubnetToWhitelistVnetResourceGroupName)
+if ($ServiceBusSku -eq 'Premium')
 {
-    $subnetResourceId = (Invoke-Executable az network vnet subnet show --resource-group $SubnetToWhitelistVnetResourceGroupName --name $SubnetToWhitelistSubnetName --vnet-name $SubnetToWhitelistVnetName | ConvertFrom-Json).id
-
-    # Add Service Endpoint to App Subnet to make sure we can connect to the service within the VNET
-    Set-SubnetServiceEndpoint -SubnetResourceId $subnetResourceId -ServiceEndpointServiceIdentifier "Microsoft.ServiceBus"
+    # Confirm if the correct parameters are passed
+    Confirm-ParametersForWhitelist -CIDR:$CIDRToWhitelist -SubnetName:$SubnetToWhitelistSubnetName -VnetName:$SubnetToWhitelistVnetName -VnetResourceGroupName:$SubnetToWhitelistVnetResourceGroupName
+    
+    # Autogenerate CIDR if no CIDR or Subnet is passed
+    
+    $CIDRToWhitelist = Get-CIDRForWhitelist -CIDR:$CIDRToWhitelist -CIDRSuffix '/32' -SubnetName:$SubnetToWhitelistSubnetName -VnetName:$SubnetToWhitelistVnetName -VnetResourceGroupName:$SubnetToWhitelistVnetResourceGroupName
+    
+    # Github issue for being able to add the same ip-addressess: https://github.com/Azure/azure-cli/issues/18479
+    # Have to add suffix because of bug: 
+    $CIDRToWhitelist = Confirm-CIDRForWhitelist -ServiceType 'servicebus' -CIDR:$CIDRToWhitelist
+    
+    # Fetch Subnet ID when subnet option is given.
+    if ($SubnetToWhitelistSubnetName -and $SubnetToWhitelistVnetName -and $SubnetToWhitelistVnetResourceGroupName)
+    {
+        $subnetResourceId = (Invoke-Executable az network vnet subnet show --resource-group $SubnetToWhitelistVnetResourceGroupName --name $SubnetToWhitelistSubnetName --vnet-name $SubnetToWhitelistVnetName | ConvertFrom-Json).id
+        
+        # Add Service Endpoint to App Subnet to make sure we can connect to the service within the VNET
+        Set-SubnetServiceEndpoint -SubnetResourceId $subnetResourceId -ServiceEndpointServiceIdentifier "Microsoft.ServiceBus"
+    }
+    
+    $optionalParameters = @()
+    if ($CIDRToWhitelist)
+    {
+        $optionalParameters += "--ip-address", "$CIDRToWhitelist"
+    }
+    elseif ($subnetResourceId)
+    {
+        $optionalParameters += "--subnet", "$subnetResourceId"
+    }
+    
+    Invoke-Executable az servicebus namespace network-rule add --name $ServiceBusNamespaceName --resource-group $ServiceBusNamespaceResourceGroupName @optionalParameters
 }
-
-$optionalParameters = @()
-if ($CIDRToWhitelist)
+else
 {
-    $optionalParameters += "--ip-address", "$CIDRToWhitelist"
+    Write-Warning "Network Whitelist only possible for Premium ServiceBus SKU. Current SKU: $ServiceBusSku"
 }
-elseif ($subnetResourceId)
-{
-    $optionalParameters += "--subnet", "$subnetResourceId"
-}
-
-Invoke-Executable az servicebus namespace network-rule add --name $ServiceBusNamespaceName --resource-group $ServiceBusNamespaceResourceGroupName @optionalParameters
 
 Write-Footer -ScopedPSCmdlet $PSCmdlet
