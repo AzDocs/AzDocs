4cfef30c3c1393a3822199fd53c8ddce170e612f
Lotte Voorhorst <BHR_VOORH503@kpn.com>
2021-07-27 08:55:55 +0000

    Merged PR 6356: Template fix
    Template fix + App Insights resource fix
    
    Tested in: https://dev.azure.com/kpn/Curiosity/_releaseProgress?_a=release-environment-logs&releaseId=17179&environmentId=42058

--- src/AppInsights/Create-AppInsights-Resource.ps1
+++ src/AppInsights/Create-AppInsights-Resource.ps1
@@ -4,8 +4,7 @@ param (
     [Parameter(Mandatory)][string] $AppInsightsResourceGroupName,
     [Alias("Location")]
     [Parameter(Mandatory)][string] $AppInsightsLocation, 
-    [Parameter()][string] $LogAnalyticsWorkspaceResourceId,
-    [Parameter(Mandatory)][string] $DiagnosticSettingLogAnalyticsWorkspaceResourceId
+    [Parameter(Mandatory)][string] $LogAnalyticsWorkspaceResourceId
 )
 
 #region ===BEGIN IMPORTS===
@@ -17,17 +16,11 @@ Write-Header -ScopedPSCmdlet $PSCmdlet
 #az monitor app-insights is in preview. Need to add extension
 Invoke-Executable az extension add --name application-insights
 
-$optionalParameters = @()
-if ($LogAnalyticsWorkspaceResourceId)
-{
-    $optionalParameters += '--workspace', "$LogAnalyticsWorkspaceResourceId"
-}
-
 # Create Application Insights
-$applicationInsightsId = (Invoke-Executable az monitor app-insights component create --app $AppInsightsName --resource-group $AppInsightsResourceGroupName --location $AppInsightsLocation @optionalParameters | ConvertFrom-Json).id
+$applicationInsightsId = (Invoke-Executable az monitor app-insights component create --app $AppInsightsName --resource-group $AppInsightsResourceGroupName --location $AppInsightsLocation --workspace $LogAnalyticsWorkspaceResourceId | ConvertFrom-Json).id
 
 # Add diagnostic settings to Application Insights
-Set-DiagnosticSettings -ResourceId $applicationInsightsId -ResourceName $AppInsightsName -LogAnalyticsWorkspaceResourceId $DiagnosticSettingLogAnalyticsWorkspaceResourceId -Metrics "[ { 'category': 'AllMetrics', 'enabled': true } ]".Replace("'", '\"')
+Set-DiagnosticSettings -ResourceId $applicationInsightsId -ResourceName $AppInsightsName -LogAnalyticsWorkspaceResourceId $LogAnalyticsWorkspaceResourceId -Metrics "[ { 'category': 'AllMetrics', 'enabled': true } ]".Replace("'", '\"')
 
 $instrumentationKey = (Invoke-Executable az resource show --resource-group $AppInsightsResourceGroupName --name $AppInsightsName --resource-type "Microsoft.Insights/components" | ConvertFrom-Json).properties.InstrumentationKey
 
