65dbb707407bbc83565fb7b284f8ab51315423ec
Lotte Voorhorst <lotte.voorhorst@kpn.com>
2021-08-10 09:33:34 +0000

    Merged PR 6509: Diagnostic setting
    fix
    
    https://dev.azure.com/kpn/Curiosity/_releaseProgress?_a=release-environment-logs&releaseId=17704&environmentId=43506
    
    https://portal.azure.com/#@kpnbv.onmicrosoft.com/resource/subscriptions/7cd65020-ab2c-46ba-956b-fe8351506430/resourceGroups/Curiosity-Testapp-dev/providers/Microsoft.Web/sites/Testapp-app/diagnosticsLogs ****

--- src/AzDocs.Common/public/DiagnosticSettings-Helper-Functions.ps1
+++ src/AzDocs.Common/public/DiagnosticSettings-Helper-Functions.ps1
@@ -28,13 +28,12 @@ function Set-DiagnosticSettings
 
     # For backwards compatibility, remove the old diagnostic settings 
     $oldDiagnosticSettings = Invoke-Executable az monitor diagnostic-settings list --resource $ResourceId | ConvertFrom-Json
-    foreach ($oldDiagnosticSetting in $oldDiagnosticSettings)
+    foreach ($oldDiagnosticSettingName in $oldDiagnosticSettings.value.name)
     {
-        # oldDiagnosticEtting.value
-        if ($oldDiagnosticSetting.value.name -and $oldDiagnosticSetting.value.name -ne $diagnosticSettingName)
+        if ($oldDiagnosticSettingName -and $oldDiagnosticSettingName -ne $diagnosticSettingName)
         {
             # remove old diagnostic setting
-            Invoke-Executable az monitor diagnostic-settings delete --name $oldDiagnosticSetting.value.name --resource $ResourceId
+            Invoke-Executable az monitor diagnostic-settings delete --name $oldDiagnosticSettingName --resource $ResourceId
         }
     }
 
