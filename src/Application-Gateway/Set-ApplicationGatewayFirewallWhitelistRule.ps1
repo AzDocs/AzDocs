4d5d50bb1a020933d95d0c9240d65da17f1b4275
Lotte Voorhorst <lotte.voorhorst@kpn.com>
2021-08-26 08:55:20 +0000

    Merged PR 6690: Custom rules for WAF
    - Improved the custom rule script for the WAF.
    - Added the possibility to also add a custom rule based on request header.
    
    Tests can be found in the attached work-item.
    
    Related work items: #19127, #19181

--- src/Application-Gateway/Set-ApplicationGatewayFirewallWhitelistRule.ps1
+++ src/Application-Gateway/Set-ApplicationGatewayFirewallWhitelistRule.ps1
@@ -3,9 +3,12 @@
 [CmdletBinding(DefaultParameterSetName = 'LowPriority')]
 param (
     [Parameter(Mandatory)][string] $IngressDomainName,
-    [Parameter(Mandatory)][string[]] $CIDRToWhitelist,
+    [Parameter()][string[]] $CIDRToWhitelist,
+    [Parameter()][string] $ApplicationGatewayWafCustomRuleRequestHeader,
+    [Parameter()][string] $ApplicationGatewayWafCustomRuleRequestHeaderValue,
     [Parameter(Mandatory)][string] $ApplicationGatewayResourceGroupName,
     [Parameter(Mandatory)][string] $ApplicationGatewayWafName,
+    [Parameter()][ValidateSet("Block", "Allow")][string] $ApplicationGatewayWafCustomRuleAction = "Block",
     [Parameter(Mandatory, ParameterSetName = 'HighPriority')][switch] $HighPriority,
     [Parameter(Mandatory, ParameterSetName = 'DefinedPriority')][ValidateRange(1, 100)][int] $Priority
 )
@@ -75,14 +78,29 @@ function Test-PriorityInUse
 }
 #endregion
 
+if (($ApplicationGatewayWafCustomRuleRequestHeader -and !$ApplicationGatewayWafCustomRuleRequestHeaderValue) -or ($ApplicationGatewayWafCustomRuleRequestHeaderValue -and !$ApplicationGatewayWafCustomRuleRequestHeader))
+{
+    throw 'Please specify both the ApplicationGatewayWafCustomRuleRequestHeader and the ApplicationGatewayWafCustomRuleRequestHeaderValue or leave them both blank.'
+}
+
 $defaultPriorityToStart = 50
 
 # Name for the rule in the WAF
 $ruleName = $IngressDomainName.ToLower().replace('.', 'DOT') + 'WhiteList'
 
-$hostCondition = New-AzApplicationGatewayFirewallCondition -MatchVariable (New-AzApplicationGatewayFirewallMatchVariable -VariableName 'RequestHeaders' -Selector 'host') -Operator Contains -MatchValue $IngressDomainName -Transform Lowercase -NegationCondition $False
+$firewallCustomRules = @(New-AzApplicationGatewayFirewallCondition -MatchVariable (New-AzApplicationGatewayFirewallMatchVariable -VariableName 'RequestHeaders' -Selector 'host') -Operator Contains -MatchValue $IngressDomainName -Transform Lowercase -NegationCondition $False)
 
-$ipCondition = New-AzApplicationGatewayFirewallCondition -Operator IPMatch -NegationCondition $true -MatchVariable ( New-AzApplicationGatewayFirewallMatchVariable -variableName 'RemoteAddr') -MatchValue $CIDRToWhitelist
+if ($CIDRToWhitelist)
+{
+    $ipCondition = New-AzApplicationGatewayFirewallCondition -Operator IPMatch -NegationCondition $true -MatchVariable (New-AzApplicationGatewayFirewallMatchVariable -variableName 'RemoteAddr') -MatchValue $CIDRToWhitelist
+    $firewallCustomRules += $ipCondition
+}
+
+if ($ApplicationGatewayWafCustomRuleRequestHeader -and $ApplicationGatewayWafCustomRuleRequestHeaderValue)
+{
+    $requestHeaderCondition = New-AzApplicationGatewayFirewallCondition -MatchVariable (New-AzApplicationGatewayFirewallMatchVariable -VariableName 'RequestHeaders' -Selector $ApplicationGatewayWafCustomRuleRequestHeader) -Operator Contains -MatchValue $ApplicationGatewayWafCustomRuleRequestHeaderValue -Transform Lowercase -NegationCondition $False
+    $firewallCustomRules += $requestHeaderCondition
+}
 
 $azPolicy = Get-AzApplicationGatewayFirewallPolicy -Name $ApplicationGatewayWafName -ResourceGroupName $ApplicationGatewayResourceGroupName
 if (!$azPolicy.CustomRules)
@@ -90,6 +108,7 @@ if (!$azPolicy.CustomRules)
     Write-Host 'Adding new custom rules collection'
     $azPolicy.CustomRules = [System.Collections.Generic.List[Microsoft.Azure.Commands.Network.Models.PSApplicationGatewayFirewallCustomRule]]::new()
 }
+
 $currentRule = $azPolicy.CustomRules | where-object Name -eq $ruleName
 
 if (!$currentRule)
@@ -123,14 +142,14 @@ if (!$currentRule)
         }
     }
     Write-Host "Adding new custom rule for $IngressDomainName"
-    $currentRule = New-AzApplicationGatewayFirewallCustomRule -Name $ruleName -Priority $toSetpriority -RuleType MatchRule -MatchCondition $hostCondition, $ipCondition -Action Block
+    $currentRule = New-AzApplicationGatewayFirewallCustomRule -Name $ruleName -Priority $toSetpriority -RuleType MatchRule -MatchCondition $firewallCustomRules -Action $ApplicationGatewayWafCustomRuleAction
     $azPolicy.CustomRules.Add($currentRule)
 }
 else
 {
-
     Write-Host "Updating settings for $IngressDomainName"
-    $currentRule.MatchConditions = $hostCondition, $ipCondition
+    $currentRule.MatchConditions = $firewallCustomRules
+    $currentRule.Action = $ApplicationGatewayWafCustomRuleAction
     switch ($PSCmdlet.ParameterSetName)
     {
         'LowPriority'
