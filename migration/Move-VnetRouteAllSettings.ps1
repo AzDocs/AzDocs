287c4c47ae9d840a7bfa4d4a7c381076593a9d65
Jacob Struiksma <jacob.struiksma@kpn.com>
2021-09-20 13:58:00 +0000

    Merged PR 6927: migration script update

--- migration/Move-VnetRouteAllSettings.ps1
+++ migration/Move-VnetRouteAllSettings.ps1
@@ -22,88 +22,75 @@ param (
     [Parameter()][switch]$WhatIf
 )
 
-function Get-AppSetRouteAll
-{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)][string]$ResourceGroupName,
-        [Parameter(Mandatory)][string]$Name,
-        [Parameter()][string]$DeploymentSlotName,
-        [Parameter(Mandatory)][ValidateSet('functionapp', 'webapp')][string]$AppType
-    )
 
-    $additionalParameters = @()
-    $DeploymentSlotNamestring = [string]::Empty
-    if ($DeploymentSlotName)
-    {
-        $additionalParameters = '--slot' , $DeploymentSlotName
-        $DeploymentSlotNamestring = "[$DeploymentSlotName]"
-    } 
-
-    Write-Host "processing $ResourceGroupName $Name$DeploymentSlotNamestring" -ForegroundColor Green
-    $configSettings = az $AppType config show --resource-group $ResourceGroupName --name $Name @additionalParameters | ConvertFrom-Json  
-    $appSettings = az $AppType config appsettings list --resource-group $ResourceGroupName --name $Name @additionalParameters | ConvertFrom-Json
-    $vnetrouteall = $appSettings | Where-Object name -EQ 'WEBSITE_VNET_ROUTE_ALL'
-
-    $collect = [PSCustomObject]@{
-        ResourceGroup       = $ResourceGroupName
-        Name                = $Name
-        needToChange        = $false
-        vnetRouteAllEnabled = $configSettings.vnetRouteAllEnabled
-        DeploymentSlotName  = $DeploymentSlotName
-    }
-
-    if ($vnetrouteall)
-    {
-        Write-Host "'WEBSITE_VNET_ROUTE_ALL' found with value $($vnetrouteall.value)"
-        if ($vnetrouteall.value -eq 1)
-        {
-            if (!$configSettings.vnetRouteAllEnabled)
-            {
-                Write-Host 'Config settings vnetRouteAllEnabled is false, need to set it on true'
-                $collect.needToChange = $true
-                $collect.vnetRouteAllEnabled = $true 
-            }
-        }
-        else
-        {
-            if ($configSettings.vnetRouteAllEnabled)
-            {
-                Write-Host 'Config settings vnetRouteAllEnabled is true, need to set it on false'
-                $collect.needToChange = $true
-                $collect.vnetRouteAllEnabled = $false 
-            }
-        }
-    }
-    else
-    {
-        Write-Host "no 'WEBSITE_VNET_ROUTE_ALL' found"
-        $collect.needToChange = $true
-        $collect.vnetRouteAllEnabled = $false 
-    }
-
-    Write-Output $collect
-    Write-Host "processed $ResourceGroupName $Name$DeploymentSlotNamestring" -ForegroundColor Green
-    if (!$DeploymentSlotName)
-    {
-        $DeploymentSlotNames = (az $AppType deployment slot list --resource-group $ResourceGroupName --name $Name | ConvertFrom-Json).name
-        if ($DeploymentSlotNames)
-        {
-            $DeploymentSlotNames | ForEach-Object {
-                Get-AppSetRouteAll -ResourceGroup $ResourceGroupName -Name $Name -DeploymentSlotName $_ -AppType:$AppType
-            }
-        }
-    }  
-}
     
 $currentAccountName = (az account show | ConvertFrom-Json).name
 Write-Host "Fetch all $AppType for '$currentAccountName'"
 $apps = az $AppType list | ConvertFrom-Json
 
 Write-Host "Getting the current state of all the $AppType"
-$appSettings = $apps | ForEach-Object {
+$appSettings = $apps | ForEach-Object -Parallel {
     $app = $_
-    Get-AppSetRouteAll -ResourceGroupName $app.ResourceGroup -Name $app.name -AppType:$AppType
+    function Get-AppSetRouteAll
+    {
+        [CmdletBinding()]
+        param (
+            [Parameter(Mandatory)][string]$ResourceGroupName,
+            [Parameter(Mandatory)][string]$Name,
+            [Parameter()][string]$DeploymentSlotName,
+            [Parameter(Mandatory)][ValidateSet('functionapp', 'webapp')][string]$AppType
+        )
+
+        $additionalParameters = @()
+        $DeploymentSlotNamestring = [string]::Empty
+        if ($DeploymentSlotName)
+        {
+            $additionalParameters = '--slot' , $DeploymentSlotName
+            $DeploymentSlotNamestring = "[$DeploymentSlotName]"
+        } 
+
+        Write-Host "processing $ResourceGroupName $Name$DeploymentSlotNamestring" -ForegroundColor Green
+        $configSettings = az $AppType config show --resource-group $ResourceGroupName --name $Name @additionalParameters | ConvertFrom-Json  
+        $appSettings = az $AppType config appsettings list --resource-group $ResourceGroupName --name $Name @additionalParameters | ConvertFrom-Json
+        $vnetrouteall = $appSettings | Where-Object name -EQ 'WEBSITE_VNET_ROUTE_ALL'
+
+        $collect = [PSCustomObject]@{
+            ResourceGroup                 = $ResourceGroupName
+            Name                          = $Name
+            needToChange                  = $false
+            CurrentVnetRouteAll           = $configSettings.vnetRouteAllEnabled
+            CurrentWEBSITE_VNET_ROUTE_ALL = $vnetrouteall.value 
+            DeploymentSlotName            = $DeploymentSlotName
+            ShouldVNetRouteAllEnabled     = $false
+        }
+
+        if (($vnetrouteall -and $vnetrouteall.value -eq 1 ) -or $configSettings.vnetRouteAllEnabled)
+        {
+            $collect.ShouldVNetRouteAllEnabled = $true
+        }
+
+        if (($collect.ShouldVNetRouteAllEnabled -ne $collect.CurrentVnetRouteAll) -or ($vnetrouteall -and $vnetrouteall.value ))
+        {
+
+            Write-Host "Config settings vnetRouteAllEnabled is $($collect.CurrentVnetRouteAll), going to set to $($collect.ShouldVNetRouteAllEnabled)"
+            Write-Host "WEBSITE_VNET_ROUTE_ALL in use: $($vnetrouteall -and $vnetrouteall.value)"
+            $collect.needToChange = $true
+        }
+
+        Write-Output $collect
+        Write-Host "processed $ResourceGroupName $Name$DeploymentSlotNamestring" -ForegroundColor Green
+        if (!$DeploymentSlotName)
+        {
+            $DeploymentSlotNames = (az $AppType deployment slot list --resource-group $ResourceGroupName --name $Name | ConvertFrom-Json).name
+            if ($DeploymentSlotNames)
+            {
+                $DeploymentSlotNames | ForEach-Object {
+                    Get-AppSetRouteAll -ResourceGroup $ResourceGroupName -Name $Name -DeploymentSlotName $_ -AppType:$AppType
+                }
+            }
+        }  
+    }
+    Get-AppSetRouteAll -ResourceGroupName $app.ResourceGroup -Name $app.name -AppType:$using:AppType
 }
 
 $appSettings | Out-GridView -Title 'apps to change - gridview'
@@ -122,7 +109,7 @@ if (!$WhatIf)
 }
 Write-Host 'Applying changes'
 
-$appSettings | ForEach-Object {
+$appSettings | Where-Object needToChange | ForEach-Object -Parallel {
     $singleAppSettings = $_
     $additionalParameters = @()
     $DeploymentSlotNamestring = [string]::Empty
@@ -131,15 +118,29 @@ $appSettings | ForEach-Object {
         $additionalParameters = '--slot' , $singleAppSettings.DeploymentSlotName
         $DeploymentSlotNamestring = "[$($singleAppSettings.DeploymentSlotName)]"
     }
-    Write-Host "$AppType $($singleAppSettings.Name)$DeploymentSlotNamestring setting vnetRouteAllEnabled '$($singleAppSettings.vnetRouteAllEnabled)'"
-    if (!$WhatIf)
+   
+    if ($singleAppSettings.ShouldVNetRouteAllEnabled -ne $singleAppSettings.CurrentVnetRouteAll)
     {
-        az $AppType config set --resource-group $singleAppSettings.ResourceGroup --name $singleAppSettings.Name --vnet-route-all-enabled $singleAppSettings.vnetRouteAllEnabled @additionalParameters
-        az $AppType config appsettings delete --resource-group $singleAppSettings.ResourceGroup --name $singleAppSettings.Name --setting-names 'WEBSITE_VNET_ROUTE_ALL' @additionalParameters
+        Write-Host "$using:AppType $($singleAppSettings.Name)$DeploymentSlotNamestring setting vnetRouteAllEnabled '$($singleAppSettings.ShouldVNetRouteAllEnabled)'"
+        if (!$using:WhatIf)
+        {
+            az $using:AppType config set --resource-group $singleAppSettings.ResourceGroup --name $singleAppSettings.Name --vnet-route-all-enabled $singleAppSettings.ShouldVNetRouteAllEnabled @additionalParameters
+        }
+        else
+        {
+            Write-Host "[WHATIF] az $using:AppType config set --resource-group $($singleAppSettings.ResourceGroup) --name $($singleAppSettings.Name) --vnet-route-all-enabled $($singleAppSettings.ShouldVNetRouteAllEnabled) $($additionalParameters)"
+        }
     }
-    else
-    { 
-        Write-Host "[WHATIF] az $AppType config set --resource-group $($singleAppSettings.ResourceGroup) --name $($singleAppSettings.Name) --vnet-route-all-enabled $($singleAppSettings.vnetRouteAllEnabled) $($additionalParameters)"
-        Write-Host "[WHATIF] az $AppType config appsettings delete --resource-group $($singleAppSettings.ResourceGroup) --name $($singleAppSettings.Name) --setting-names 'WEBSITE_VNET_ROUTE_ALL' $($additionalParameters)"
+    if ($singleAppSettings.CurrentWEBSITE_VNET_ROUTE_ALL)
+    {
+        Write-Host "$using:AppType $($singleAppSettings.Name)$DeploymentSlotNamestring removing 'WEBSITE_VNET_ROUTE_ALL' setting"
+        if (!$using:WhatIf)
+        {
+            az $using:AppType config appsettings delete --resource-group $singleAppSettings.ResourceGroup --name $singleAppSettings.Name --setting-names 'WEBSITE_VNET_ROUTE_ALL' @additionalParameters
+        }
+        else
+        {
+            Write-Host "[WHATIF] az $using:AppType config appsettings delete --resource-group $($singleAppSettings.ResourceGroup) --name $($singleAppSettings.Name) --setting-names 'WEBSITE_VNET_ROUTE_ALL' $($additionalParameters)"
+        }
     }
 }
