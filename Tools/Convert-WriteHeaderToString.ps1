e75e8dc496dbe93e3dab104fa2eb2df3cbfa7f81
Jacob Struiksma <jacob.struiksma@kpn.com>
2021-11-22 09:35:30 +0000

    Merged PR 7570: tooling for azdocs: write-header to parameters
    init checkin

---
+++ Tools/Convert-WriteHeaderToString.ps1
@@ -0,0 +1,47 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> Convert-WriteHeaderToString -inputLine @'
+>  ParameterSetName : DeploymentSlot
+>  AppServicePlanName : ASP-dev-Linux
+>  AppServicePlanResourceGroupName : ASP-dev
+>  FunctionAppResourceGroupName : app-dev
+'@
+    Converts the output of write-header to arguments like you would be able to call the script itself
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+function Convert-WriteHeaderToString
+{
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $inputLine
+    )
+
+    $lines = $inputLine.Split("`n", [StringSplitOptions]::RemoveEmptyEntries)
+    $lines | ForEach-Object {
+        $line = $_.TrimStart('>').Trim()
+        if (!$line.StartsWith('ParameterSetName'))
+        {
+            $sep = $line.IndexOf(':')
+            $parameter = $line.Substring(0, $sep).Trim()
+            $value = $line.Substring($sep + 1).Trim();
+            $newValue = switch ($value)
+            {
+                'True' { '$true' }
+                'False' { '$false' }
+                Default { "'$value'" }
+            }
+            "-$parameter $newValue"
+        }
+    } | Join-String -Separator ' '
+}
