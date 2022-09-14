ac945543864a37b6a7bfcb051e5eec59180c835b
Jacob Struiksma <jacob.struiksma@kpn.com>
2022-09-14 13:51:30 +0200

    fix giving tags to invoke executable

--- src/Resourcegroup/Create-ResourceGroup.ps1
+++ src/Resourcegroup/Create-ResourceGroup.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param (
-    [Alias("Location")]
+    [Alias('Location')]
     [Parameter(Mandatory)][string] $ResourceGroupLocation,
     [Parameter(Mandatory)][string] $ResourceGroupName,
     [Parameter()][string[]] $ResourceTags
@@ -11,10 +11,11 @@ Import-Module "$PSScriptRoot\..\AzDocs.Common" -Force
 #endregion ===END IMPORTS===
 
 Write-Header -ScopedPSCmdlet $PSCmdlet
-$response =invoke-executable az group create --location $ResourceGroupLocation --name $ResourceGroupName --tags $ResourceTags 
+$response = Invoke-Executable az group create --location $ResourceGroupLocation --name $ResourceGroupName --tags @ResourceTags 
 $resourceGroupId = ( $response | ConvertFrom-Json).id
 
-if(!$resourceGroupId){
+if (!$resourceGroupId)
+{
     throw 'Could not create the resource group'
 }
 # Update Tags
