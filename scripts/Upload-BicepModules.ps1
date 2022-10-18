58fa3159db5dd184929fecddfcecb8fb020b87aa
Jacob Struiksma <jacob.struiksma@kpn.com>
2022-10-18 10:48:35 +0000

    Merged PR 13555: execute upload bicep modules parallel and move folders around
    execute upload bicep modules parallel and move folders around

--- scripts/Upload-BicepModules.ps1
+++ scripts/Upload-BicepModules.ps1
@@ -6,39 +6,55 @@
 .NOTES
   The manual way to publish is for example: 'az bicep publish --file src-bicep\Storage\storageAccounts.bicep --target br:$registryName.azurecr.io/storage/storageaccounts.bicep:latest'.
 .EXAMPLE
-    . .\scripts\Upload-BicepModules.ps1
+    $env:BUILD_SOURCEBRANCHNAME = 'test_branch'
+    $env:BUILD_BUILDNUMBER = (Get-Date -Format "yyyy.MM.dd.1-") + $env:BUILD_SOURCEBRANCHNAME
+
+    . .\scripts\Upload-BicepModules.ps1 -registryName 'acrazdocsdev'
+.EXAMPLE
+    $env:BUILD_SOURCEBRANCHNAME = 'main'
+    $env:BUILD_BUILDNUMBER = (Get-Date -Format "yyyy.MM.dd.1-") + $env:BUILD_SOURCEBRANCHNAME
+
+    . .\scripts\Upload-BicepModules.ps1 -registryName 'wooooooooooot'    
   #>
 param(
 
     # Name of the Azure Container Registry
+    [Parameter(Mandatory)]
+    [string]$RegistryName,
+
     [Parameter()]
-    [string]$registryName
+    [string]$SourceBranchName = $env:BUILD_SOURCEBRANCHNAME,
+
+    [Parameter()]
+    [string]$TagName = $env:BUILD_BUILDNUMBER
+
 )
 
 Push-Location
 try
 {
     
-    Write-Host "Branch $($env:BUILD_SOURCEBRANCHNAME)"
-   
-
+    Write-Host "Branch $SourceBranchName"
     Set-Location .\src-bicep
-    Get-ChildItem -Recurse -Path '*.bicep' | ForEach-Object { 
+    Get-ChildItem -Recurse -Path '*.bicep' | ForEach-Object -ThrottleLimit 16 -Parallel  { 
         $bicepFile = $_
+        $localTagName = $using:TagName
+        $localRegistryName = $using:RegistryName
+        $localSourceBranchName = $using:SourceBranchName
         Write-Host "Bicep file: $bicepFile"
 
         $relativePath = Resolve-Path $bicepFile.Directory -Relative
         $repositoryName = (Join-Path -Path $relativePath.TrimStart('.', '\', '/') -ChildPath $bicepFile.BaseName).ToLowerInvariant().Replace('\', '/')
 
-        $repositoryUrl = "$registryName.azurecr.io/$repositoryName"
+        $repositoryUrl = "$localRegistryName.azurecr.io/$repositoryName"
         Write-Host "Registry url $repositoryUrl"
-        if ($env:BUILD_SOURCEBRANCHNAME -eq 'main')
+        if ($localSourceBranchName -eq 'main')
         {
             Write-Host 'Pushing latest tag'
-            az bicep publish --file $bicepFile --target "br:$($repositoryUrl):latest"
+           az bicep publish --file $bicepFile --target "br:$($repositoryUrl):latest"
         }
-        Write-Host "Pushing tag $($env:BUILD_BUILDNUMBER)"
-        az bicep publish --file $bicepFile --target "br:$($repositoryUrl):$env:BUILD_BUILDNUMBER"
+        Write-Host "Pushing tag $localTagName"
+        az bicep publish --file $bicepFile --target "br:$($repositoryUrl):$localTagName"
     }
 }
 finally
